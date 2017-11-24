5dd30f410c16240d219d6f756280949a885c52d5
George Zakharov <george_zakharov@mail.ru>
2017-11-24 12:58:16 +0300

    refactoring

--- Core/CreditCardValidator.php
+++ Core/CreditCardValidator.php
@@ -2,22 +2,22 @@
 class CreditCardValidator
 {
 	public $result = [
-		'cardType' 			=> 'unknown',
-		'validationMessage' => 'No data'
+		'cardType'		=> 'unknown',
+		'validationMessage'	=> 'No data'
 	];
 
 	private $codeMatchPatterns = [
-		'American Express' 			  => '/^34|37/',
-		'Diners Club - Carte Blanche' => '/^300|301|302|303|304|305/',
-		'Diners Club - International' => '/^36/',
-		'Diners Club - USA & Canada'  => '/^54/',
-		'Discover' 					  => '/^6011|(622126-622925)|644|645|646|647|648|649|65/',
-		'InstaPayment' 				  => '/^637|638|639/',
-		'JCB' 						  => '/^(3528-3589)/',
-		'Maestro' 					  => '/^(5018|5020|5038|5893|6304|6759|6761|6762|6763)/',
-		'MasterCard' 				  => '/^51|52|53|54|55|(222100-272099)/',
-		'Visa Electron' 			  => '/^4026|417500|4508|4844|4913|4917/',
-		'Visa' 						  => '/^4/'
+		'American Express'		=> '/^34|37/',
+		'Diners Club - Carte Blanche'	=> '/^300|301|302|303|304|305/',
+		'Diners Club - International'	=> '/^36/',
+		'Diners Club - USA & Canada'	=> '/^54/',
+		'Discover'			=> '/^6011|(622126-622925)|644|645|646|647|648|649|65/',
+		'InstaPayment'			=> '/^637|638|639/',
+		'JCB'				=> '/^(3528-3589)/',
+		'Maestro'			=> '/^(5018|5020|5038|5893|6304|6759|6761|6762|6763)/',
+		'MasterCard'			=> '/^51|52|53|54|55|(222100-272099)/',
+		'Visa Electron'			=> '/^4026|417500|4508|4844|4913|4917/',
+		'Visa'				=> '/^4/'
 	];
 
 	public function __construct()
